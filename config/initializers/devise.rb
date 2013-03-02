989cae110ac847eb42ad452cd6e4be701d27e4ee
vincentmilliken <vincentamilliken@gmail.com>
2013-03-02 16:54:09 +0000

    removed github login stuff

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -213,7 +213,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, '65c3c68049b714dd52dd', 'b1f34333d8647c9d84255e67d9bc78e7c99b9594', :scope => 'user,public_repo'
+  # config.omniauth :github, '65c3c68049b714dd52dd', 'b1f34333d8647c9d84255e67d9bc78e7c99b9594', :scope => 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
