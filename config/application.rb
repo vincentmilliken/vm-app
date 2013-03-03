e043aabe3c562395ea44c1bdad91ca87fe06a44d
vincentmilliken <vincentamilliken@gmail.com>
2013-03-03 23:48:50 +0000

    trying to deploy this app

--- config/application.rb
+++ config/application.rb
@@ -57,8 +57,6 @@ module VmApp
     config.assets.enabled = true
 
     config.assets.initialize_on_precompile = false
-
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
